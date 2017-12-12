cb5226a2d3699d3ebf9dd18abb9ba7832b9f1c4f
pjakapan <j.petchan@togetherteam.co.th>
2017-12-12 19:24:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nightwatch-t
+
+clone repo already!
+
+
+cmd : npm install // install modules via commandline
+
+
+cmd : npm test // try to testing and get reports
+
+reporter : nightwatch-html-custom-reporter
---
+++ html-reporter.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var path = require('path');
+var handlebars = require('handlebars');
+var dateFormat = require('dateformat');
+
+module.exports = {
+    write: function (results, options, done) {
+        
+        var keys = Object.keys(results.modules);
+        var moduleKey = keys.shift();
+        var pathParts = moduleKey.split(path.sep);
+        var moduleName = pathParts.pop();
+        var now = new Date();
+        var reportFilename = moduleName + '_' + options.filename_prefix + dateFormat(now, "yyyymmdd_HHMMss") + '.html';
+        var reportFilePath = path.join(__dirname, options.output_folder, reportFilename);
+
+        // read the html template
+        fs.readFile('./bin/htmlReporter/template/html-reporter.hbs', function (err, data) {
+            if (err) throw err;
+
+            var template = data.toString();
+
+            // merge the template with the test results data
+            var html = handlebars.compile(template)({
+                results: results,
+                options: options,
+                timestamp: new Date().toString(),
+                browser: options.filename_prefix.split('_').join(' '),
+                reportName: reportFilename
+            });
+
+            // write the html to a file
+            fs.writeFile(reportFilePath, html, function (err) {
+                if (err) throw err;
+                console.log('Report generated: ' + reportFilePath);
+                done();
+            });
+        });
+    }
+};
